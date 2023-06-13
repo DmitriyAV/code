aaac9a6a343a7b7fdc83dc1500f2d126d8f97857
DmitryVM <dima.alekceev@gmail.com>
2023-06-13 18:52:42 +0300

    add css grid homework

--- bootcamp/exercises/week-1/function/functionGalor.js
+++ bootcamp/exercises/week-1/function/functionGalor.js
@@ -1,72 +1,72 @@
 people_info = [
-  {
-    name: "guido",
-    profession: "bungalow builder",
-    age: 17,
-    country: "canaland",
-    city: "sydurn",
-    catchphrase: "what a piece of wood!"
-  },
-  {
-    name: "petra",
-    profession: "jet plane mechanic",
-    age: 31,
-    country: "greenmark",
-    city: "bostork",
-    catchphrase: "that's my engine, bub"
-  },
-  {
-    name: "damian",
-    profession: "nursery assistant",
-    age: 72,
-    country: "zimbia",
-    city: "bekyo",
-    catchphrase: "with great responsibility comes great power"
-  }
+    {
+        name: "guido",
+        profession: "bungalow builder",
+        age: 17,
+        country: "canaland",
+        city: "sydurn",
+        catchphrase: "what a piece of wood!"
+    },
+    {
+        name: "petra",
+        profession: "jet plane mechanic",
+        age: 31,
+        country: "greenmark",
+        city: "bostork",
+        catchphrase: "that's my engine, bub"
+    },
+    {
+        name: "damian",
+        profession: "nursery assistant",
+        age: 72,
+        country: "zimbia",
+        city: "bekyo",
+        catchphrase: "with great responsibility comes great power"
+    }
 ]
 
 
 const capitalize = function (str) {
-  let capitalizedStr = ""
-  capitalizedStr += str[0].toUpperCase(); //add the first letter of the str, capitalized
-  capitalizedStr += str.slice(1) //add the rest of str normallyreturn capitalizedStr
-  return capitalizedStr
+    let capitalizedStr = ""
+    capitalizedStr += str[0].toUpperCase(); //add the first letter of the str, capitalized
+    capitalizedStr += str.slice(1) //add the rest of str normallyreturn capitalizedStr
+    return capitalizedStr
 }
 
-const getAge = function(age) {
-  const underage = 21
-  const oldtimer = 55
+const getAge = function (age) {
+    const underage = 21
+    const oldtimer = 55
 
-  if (age <= underage) {
-    return "Underage"
-   } else if (person.age >= oldtimer) {
-     return "55+"
-   }
+    if (age <= underage) {
+        return "Underage"
+    } else if (person.age >= oldtimer) {
+        return "55+"
+    }
 
 }
 
-const wrapCatchphrase = function(str) {
-  let wrap = ""
+const wrapCatchphrase = function (str) {
+    let wrap = ""
 
-  wrap += `"${str}"`
+    wrap += `"${str}"`
 
-  return wrap
+    return wrap
 }
 
 const getSummary = function (person) {
 
-  let space = "\n"
-  let filter = ""
+    let space = "\n"
+    let filter = ""
 
-     filter += capitalize(person.name) + space
-     filter += getAge(person.age) + space
-     filter += capitalize(person.profession) + space
-     filter += capitalize(person.country) + ":" + space
-     filter += capitalize(person.city) + space
-     filter += wrapCatchphrase(capitalize(person.catchphrase)) + space
+    filter += capitalize(person.name) + space
+    filter += getAge(person.age) + space
+    filter += capitalize(person.profession) + space
+    filter += capitalize(person.country) + ":" + space
+    filter += capitalize(person.city) + space
+    filter += wrapCatchphrase(capitalize(person.catchphrase)) + space
 
-   
-  return filter
+
+    return filter
 
 }
 
@@ -77,30 +77,32 @@ console.log(getSummary(people_info[0]))
 //SecondPart
 
 const story = "In the beginning there was light. Then there were wolves. Finally there was a big fire. Ultimately, Shelob the wolf-master put out the fire with her feet. But until then, the fire caused one heck of a lot of damage."
-const specialChars = [",", ".", "'", '"',"?", "!", ";"]
-const wordCounts = {}
+const specialChars = [",", ".", "'", '"', "?", "!", ";"]
 
-const IsExsist = function(str, str1) {
-  if (str === str1) {
-    return true
-  } else return false
+const iterateWordsStore = function (arr, str) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === str) {
+            return true
+        }
+    }
 }
 
-const wordCounter = function(phrase) {
+const wordCounter = function (phrase) {
 
-let wordStore = {}
-let desired = phrase.replace(/[^\w\s]/gi, '')
-let splitted = desired.split(/[ ]/)
+    let wordStore = {}
+    let desired = phrase.replace(/[^\w\s]/gi, '')
+    let desireLow = desired.toLowerCase()
+    let arrayWords = desireLow.split(' ')
 
-
-for (let i = 0; i < splitted.lenght; i++) {
-  if(!IsExsist(splitted[i], wordStore[i])) {
-      wordStore[i] = word
-      
-  } 
+    for (let word of arrayWords) {
+        let keyStoredWords = Object.keys(wordStore)
+        if (iterateWordsStore(keyStoredWords, word)) {
+            wordStore[word] += 1
+        } else {
+            wordStore[word] = 1
+        }
+    }
+    return wordStore
 }
-  return wordStore
-}
-
 
 console.log(wordCounter(story))
--- bootcamp/exercises/week-2/JsThis/main.js
+++ bootcamp/exercises/week-2/JsThis/main.js
@@ -10,7 +10,7 @@
 
 //     }
 //   }
-// }  
+// }
 
 // person.feed() //should alert "I'm no longer hungry"
 
@@ -41,7 +41,7 @@
 //   console.log('Mercedes should have 31 liters: ', garage.car2.liters);
 
 
-//Excercise 3 
+//Excercise 3
 
 // const pumpFuel = function (plane) {
 //     plane.fuel += 1;
@@ -73,99 +73,117 @@
 const tipJar = {
     coinCount: 20,
     tip: function () {
-      this.coinCount += 1;
+        this.coinCount += 1;
     },
-    stealCoins: function(num) {
+    stealCoins: function (num) {
         this.coinCount -= num;
     }
-  };
+};
 
-  tipJar.tip();
-  console.log('Tip jar should have 21 coins: ' + tipJar.coinCount);
+tipJar.tip();
+console.log('Tip jar should have 21 coins: ' + tipJar.coinCount);
 
-  tipJar.stealCoins(3);
-  console.log('Tip jar should have 18 coins: ' + tipJar.coinCount);
+tipJar.stealCoins(3);
+console.log('Tip jar should have 18 coins: ' + tipJar.coinCount);
 
-  tipJar.stealCoins(10);
-  console.log('Tip jar should have 8 coins: ' + tipJar.coinCount);
+tipJar.stealCoins(10);
+console.log('Tip jar should have 8 coins: ' + tipJar.coinCount);
 
-//Exercise 5 
+//Exercise 5
 
 const revealSecret = function () {
     return this.secret;
-  };
+};
 
-  const shoutIt = function (person, func) {
+const shoutIt = function (person, func) {
     person.revealItAll = func;
     const result = person.revealItAll();
     alert(person.name + " said: " + result);
-  };
+};
 
-  const avi = {
+const avi = {
     name: "Avi",
     secret: "Im scared of snakes!"
-  };
+};
 
-  const narkis = {
+const narkis = {
     name: "Narkis",
     secret: "I dont have secrets because I'm zen like that."
-  };
+};
 
-  shoutIt(avi, revealSecret);
-  shoutIt(narkis, revealSecret);
+shoutIt(avi, revealSecret);
+shoutIt(narkis, revealSecret);
 
-//Exercise 6 
+//Exercise 6
 
 
 const coffeeShop = {
+    money: 1000,
     beans: 40,
+    priceOfBeans: 10,
 
     drinkRequirements: {
-        latte: 10,
-        americano: 5,
-        doubleShot: 15,
-        frenchPress: 12
+        latte: {beanRequirement: 10, price: 5},
+        americano: {beanRequirement: 5, price: 5},
+        doubleShot: {beanRequirement: 15, price: 5},
+        frenchPress: {beanRequirement: 12, price: 5}
+    },
+
+    checkOnZero: function (num) {
+        let sum = coffeeShop.beans - num
+        if (sum <= 0) {
+            alert('Sorry, we’re all out of beans!')
+        } else return false;
+    },
+
+    buyBeans: function (numBeans) {
+        let price = numBeans * this.priceOfBeans
+        if (price < this.money) {
+            console.log(`${this.money} isn't enough for bought!`)
+
+        } else {
+            this.beans += numBeans
+            this.money -= price
+            console.log(`We bought ${numBeans} quality of beans!`)
+        }
     },
 
     makeDrink: function (drinkType) {
 
-        if (drinkType in this.drinkRequirements) {
-            switch (drinkType) {
-                case "latte":
-                    checkonZero(this.drinkRequirements.latte)
-                    this.beans -= this.drinkRequirements.latte
-                    break;
+        if (!(drinkType in this.drinkRequirements)) {
+            alert("Sorry, we don’t make ")
 
-                case "americano":
-                    checkonZero(this.drinkRequirements.americano)
-                    this.beans -= this.drinkRequirements.americano
-                    break;
+        } else {
+            this.beans -= this.drinkRequirements[drinkType]
+            console.log(`There is your order: ${drinkType}`)
+        }
+    },
 
-                case "doubleShot":
-                    checkonZero(this.drinkRequirements.doubleShot)
-                    this.beans -= this.drinkRequirements.doubleShot
-                    break;
+    buyDrink: function (drinkType) {
+        if (!this.checkOnZero(this.drinkRequirements[drinkType])) {
+            this.makeDrink(drinkType)
+        } else {
+            this.buyBeans(this.drinkRequirements[drinkType.beans] * 2)
+        }
+    },
 
-                case "frenchPress":
-                    checkonZero(this.drinkRequirements.frenchPress)
-                    this.beans -= this.drinkRequirements.frenchPress
-                    break;
-
-
-            }
-        } else { alert("Sorry, we don’t make ") }
+    order: function (money, drinkType) {
+        let order = this.drinkRequirements[drinkType.price]
+        if (money < order) {
+            console.log("There is not enough money!")
+        } else if (money > order) {
+            let cheange = money - order
+            let buying = order - money
+            this.money += buying
+            console.log(`There is your change: ${cheange}`)
+            this.buyDrink(drinkType)
+        }
     }
 }
 
-const checkonZero = function (num) {
-    let sum = coffeeShop.beans - num
-    if ( sum <= 0 ) {
-        alert('Sorry, we’re all out of beans!')
-    } else return;
-}
 
-coffeeShop.makeDrink("latte");
-coffeeShop.makeDrink("americano");
-coffeeShop.makeDrink("filtered"); //should alert/console "Sorry, we don't make filtered"
-coffeeShop.makeDrink("doubleShot");
-coffeeShop.makeDrink("frenchPress"); //should alert/console "Sorry, we're all out of beans"
+coffeeShop.order(10, "latte");
+// coffeeShop.makeDrink("americano");
+// coffeeShop.makeDrink("filtered"); //should alert/console "Sorry, we don't make filtered"
+// coffeeShop.makeDrink("doubleShot");
+// coffeeShop.makeDrink("frenchPress"); //should alert/console "Sorry, we're all out of beans"
